e4539fdc6439607f8f18fdad376d4bd7642620ac
yogiraj99 <ytambake@gmail.com>
2019-02-07 11:10:59 +0530

    Connect to database using connectionstring

--- node_app/app.js
+++ node_app/app.js
@@ -1,21 +1,12 @@
 const express = require('express');
-const app = express();
-
 const { Client } = require('pg');
+const connectionString = process.env.CONNETIONSTRING;
 
-const client = new Client({
-  user: 'postgres',
-  host: '172.18.0.2',
-  database: 'postgres',
-  password: 'postgres',
-  port: 5432,
-});
-
+const client = new Client({ connectionString: connectionString });
 client.connect().then(() => console.log('connected')).catch(err => console.error('connection error', err.stack));
 
-
 const postNumber = function (req, res) {
-  console.log(`added number ${req.body.number}`);
+  console.log(`adding number ${req.body.number}`);
   client.query('INSERT INTO numbers values ($1)', [req.body.number], (err, pgRes) => {
     console.log(err ? err.stack : pgRes);
     res.send(`added number ${req.body.number}`);
@@ -40,6 +31,8 @@ const getAllNumbers = function (req, res) {
     res.send(responseBody);
   });
 };
+
+const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.post('/number', postNumber);
